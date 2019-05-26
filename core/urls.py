6c8dbf995341829f943bca4cef1b47dc8fc3244f
pneha1234 <nehapandey408@gmail.com>
2019-05-26 08:17:11 +0545

    gallery page created

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 
 urlpatterns =[
+   
     path('home/', views.HomeView.as_view(), name='home'),
     path('members/', views.MembersView.as_view(), name='members'),
     path('base/', views.base_view.as_view(), name='base'),
+    path('gallery/', views.GalleryView.as_view(), name='gallery'),
+
 
 ]
 
