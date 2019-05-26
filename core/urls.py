60226a4c2b653ba248379a6d48b80b8ee016e79b
pneha1234 <nehapandey408@gmail.com>
2019-05-26 08:44:26 +0545

    events page created

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ urlpatterns =[
     path('members/', views.MembersView.as_view(), name='members'),
     path('base/', views.base_view.as_view(), name='base'),
     path('gallery/', views.GalleryView.as_view(), name='gallery'),
+    path('events/', views.EventsView.as_view(), name='events'),
 
 
 ]
